25b3e257e08d71424ea161415345ddce79f41217
ashrafmahmod <102529128+ashrafmahmod@users.noreply.github.com>
2025-05-31 17:51:02 +0300

    Add files via upload

--- src/utiities/request.js
+++ src/utiities/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const request = axios.create({
   // baseURL: "http://localhost:8000", // local host
-  baseURL: "https://bloggeryoussef-production.up.railway.app/", // railway server
+  baseURL: "https://bloggeryoussef-production.up.railway.app", // rail way
 });
 
 export default request;
